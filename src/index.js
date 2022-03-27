6d20756b587d4d1aefa1c7cf32d69b283e009473
Jay Cruz <49811999+CoderJay06@users.noreply.github.com>
2022-03-27 03:42:19 -0400

    Add character data and setup render

--- src/index.js
+++ src/index.js
@@ -1,24 +1,46 @@
+/*
+  import data file for characters data
+  create a class for making each charcater
+*/
+import data from "../src/character_data.json";
+
+class Character {
+  constructor(data) {
+    Object.assign(this, data);
+    this.diceHtml = this.getDiceHtml(this.diceCount);
+  }
+  // add function that returns a random number 1 - 6
+  _getRandomNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  // add a getDice function for creating array of dice
+  // (diceCount in length) parameter: diceCOunt
+  getDiceHtml(diceCount) {
+    return new Array(diceCount)
+      .fill(0)
+      .map(() => `<div class="dice">${this._getRandomNumber()}</div>`)
+      .join("");
+  }
+}
+
+function getCharacterHtml(character) {
+  const { name, avatarImg, health, diceHtml } = character;
+  return `
+    <div class="character-card">
+          <h4 class="name">${name}</h4>
+          <img class="avatar" src=${avatarImg}/>
+          <p class="health">health: <b>${health}</b></p>
+          <div class="dice-container">${diceHtml}</div>
+      </div>
+    `;
+}
+
 function render() {
-  document.getElementById("hero").innerHTML = getHeroHtml();
-  document.getElementById("monster").innerHTML = getMonsterHtml();
-}
-
-function getHeroHtml() {
-  return `<div class="character-card">
-          <h4 class="name"> Wizard </h4>
-          <img class="avatar" src="images/wizard.png"/>
-          <p class="health">health: <b> 60 </b></p>
-          <div class="dice-container"><div class="dice"> 6 </div></div>
-      </div>`;
-}
-
-function getMonsterHtml() {
-  return `<div class="character-card">
-          <h4 class="name"> Orc </h4>
-          <img class="avatar" src="images/orc.png"/>
-          <p class="health">health: <b> 10 </b></p>
-          <div class="dice-container"><div class="dice"> 4 </div></div>
-      </div>`;
+  let hero = new Character(data.wizard);
+  let monster = new Character(data.orc);
+  document.getElementById("hero").innerHTML = getCharacterHtml(hero);
+  document.getElementById("monster").innerHTML = getCharacterHtml(monster);
 }
 
 render();
